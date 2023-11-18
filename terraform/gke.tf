4c7308b5e006fb91f76cd21c51dc8cc696b9b617
Shion Ichikawa <shion1305@gmail.com>
2023-11-18 20:13:45 +0900

    terraform configuration

---
+++ terraform/gke.tf
@@ -0,0 +1,59 @@
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 1
+  description = "number of gke nodes"
+}
+
+# GKE cluster
+data "google_container_engine_versions" "gke_version" {
+  location       = var.region
+  version_prefix = "1.27."
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "${var.project_id}-gke"
+  location = var.region
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  name           = google_container_cluster.primary.name
+  location       = var.region
+  cluster        = google_container_cluster.primary.name
+  node_locations = ["${var.region}-a", "${var.region}-b"]
+  version        = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
+  node_count     = var.gke_num_nodes
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    machine_type = "g1-small"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    spot         = true
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+  }
+}
+
